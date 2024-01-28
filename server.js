8c220108f0ccf28afea243b6e282021238e6b981
Zaid Shaikh <s.zaidshaikh99@gmail.com>
2024-01-28 20:37:49 +0530

    fix: resolved cors issue

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const hotelRouter = require("./routes/hotel.router");
 const categoryRouter = require("./routes/category.router");
 const hotelDataAddedToDBRouter = require("./routes/dataimport.router");
@@ -8,9 +10,12 @@ const singleHotelRouter = require("./routes/singleHotel.router");
 const authRouter = require("./routes/auth.router");
 const wishlistRouter = require("./routes/wishlist.router");
 const connectDB = require("./config/dbconfig");
+
 const app = express();
 const PORT = 3500;
 app.use(express.json());
+connectDB();
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the landing page");
@@ -18,14 +23,12 @@ app.get("/", (req, res) => {
 
 app.use("/api/hoteldata", hotelDataAddedToDBRouter);
 app.use("/api/categorydata", categoryDataAddedToDBRouter);
-
 app.use("/api/hotels", hotelRouter);
 app.use("/api/category", categoryRouter);
 
 app.use("/api/hotels", singleHotelRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/wishlist", wishlistRouter);
-connectDB();
 
 mongoose.connection.once("open", () => {
   console.log("Connected to DB");
